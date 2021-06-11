281cd9e8f322e1af9792e3e9858730ded38e87d1
Jef Dewitt <jef.dewitt@twinspires.com>
2021-06-11 09:56:00 -0400

    Adds random user service.

--- da-web/src/components/Profile/Profile.js
+++ da-web/src/components/Profile/Profile.js
@@ -1,31 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import UserService from '../../services/UserService';
 import './Profile.css';
 
-const Profile = () => (
-  <div className="Profile">
-    <h1>Profile Component</h1>
-    <div className="profile-img-cont">
-      <img src="" alt=""></img>
-    </div>
-    <div className="profile-icons">
-      <ul>
-        <li>icon 1</li>
-        <li>icon 2</li>
-        <li>icon 3</li>
-        <li>icon 4</li>
-        <li>icon 5</li>
-        <li>icon 6</li>
-      </ul>
-    </div>
-    <div className="profile-paragraph">
-      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt msollit anim id est laborum."</p>
-    </div>
-  </div>
-);
+class Profile extends Component {
 
-Profile.propTypes = {};
+  constructor() {
+    super();
+    this.state = {
+      isLoaded: false,
+      userData: null,
+      error: null
+    }
+  }
 
-Profile.defaultProps = {};
+  // Gets called after first render
+  componentDidMount() {
+    // Get random user data
+    UserService.getRandoUser()
+      .then(result => {
+        console.log(result)
+        this.setState({
+          isLoaded: true,
+          userData: result
+        })
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.error) {
+      return <div>Error: {this.state.error.message}</div>;
+    } else if (!this.state.isLoaded) {
+      return (<p>Loading...</p>)
+    } else {
+      return (
+        <div className="Profile">
+        <h1>Profile Component</h1>
+        <div className="profile-img-cont">
+          <img src={this.state.userData.results[0].picture.large} alt=""></img>
+        </div>
+        <div className="profile-icons">
+          <ul>
+            <li>icon 1</li>
+            <li>icon 2</li>
+            <li>icon 3</li>
+            <li>icon 4</li>
+            <li>icon 5</li>
+            <li>icon 6</li>
+          </ul>
+        </div>
+        <div className="profile-paragraph">
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt msollit anim id est laborum."</p>
+        </div>
+      </div>
+      )
+    }
+  }
+}
 
 export default Profile;
---
+++ da-web/src/services/UserService.js
@@ -0,0 +1,14 @@
+
+const randomUserUrl = "https://randomuser.me/api";
+
+const UserService = {
+
+     // Get random user
+     getRandoUser: async function() {
+        const response = await fetch(randomUserUrl, {mode: 'cors'});
+        const json = await response.json();
+        return json;
+    },
+}
+
+export default UserService
