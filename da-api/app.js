bf44c6dc00ea878aae7763e6d2ab7157fdeceff0
Jef Dewitt <jef.dewitt@twinspires.com>
2021-06-11 16:16:58 -0400

    File cleanup.

--- da-api/app.js
+++ da-api/app.js
@@ -1,13 +1,36 @@
+require('dotenv').config();
 var createError = require('http-errors');
+const cors = require('cors');
 var express = require('express');
 var path = require('path');
+const bodyParser= require('body-parser'); // parses form data & incoming req bodies from req.body
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let port = 4200;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var listener = app.listen(parseInt(process.env.PORT, 10), function(){
+  console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+});
+
+// allow CORS
+app.use('/img', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  next();
+})
+// found the above code wasn't cutting it so I employed the following two lines
+app.use(cors());
+app.options('*', cors());
+
+app.use(bodyParser.json()); // handle JSON request bodies
+app.use(bodyParser.urlencoded({ extended: true })); // handle x-www-form-urlencoded request bodies
+app.use(cookieParser());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ da-api/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+// Define user (email sender) schema
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true, index: true},
+    email: { type: String, required: true},
+    message: { type: String, required: true }
+})
+mongoose.model('users', userSchema);
