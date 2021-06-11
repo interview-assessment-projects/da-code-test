7d0ab28fa7d5e56fbdb9acafd43cdd94b005d9e6
Jef Dewitt <jef.dewitt@twinspires.com>
2021-06-11 10:23:49 -0400

    Wires up NavBar social anchor tags to external sites.

--- da-web/src/components/NavBar/NavBar.js
+++ da-web/src/components/NavBar/NavBar.js
@@ -6,9 +6,9 @@ import './NavBar.css';
 const NavBar = () => (
   <div className="NavBar">
     <ul>
-        <li> <FaFacebookF/> </li>
-        <li> <FaTwitter/> </li>
-        <li> <FaInstagram/> </li>
+        <li> <a href="https://www.facebook.com" target="_blank"><FaFacebookF/></a> </li>
+        <li> <a href="https://www.twitter.com" target="_blank"><FaTwitter/></a> </li>
+        <li> <a href="https://www.instagram.com" target="_blank"><FaInstagram/></a> </li>
       </ul>
   </div>
 );
