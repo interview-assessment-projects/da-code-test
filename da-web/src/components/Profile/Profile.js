418cd1110d2c190e0a0c343dc1d7933ea921615d
Jef Dewitt <jef.dewitt@twinspires.com>
2021-06-11 08:12:48 -0400

    Stubs out frontend - mobile layout.

--- da-web/src/components/Profile/Profile.js
+++ da-web/src/components/Profile/Profile.js
@@ -5,9 +5,22 @@ import './Profile.css';
 const Profile = () => (
   <div className="Profile">
     <h1>Profile Component</h1>
-    <div className="profile-img"></div>
-    <div className="desc-icons"></div>
-    <div className="desc-paragraph"></div>
+    <div className="profile-img-cont">
+      <img src="" alt=""></img>
+    </div>
+    <div className="profile-icons">
+      <ul>
+        <li>icon 1</li>
+        <li>icon 2</li>
+        <li>icon 3</li>
+        <li>icon 4</li>
+        <li>icon 5</li>
+        <li>icon 6</li>
+      </ul>
+    </div>
+    <div className="profile-paragraph">
+      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt msollit anim id est laborum."</p>
+    </div>
   </div>
 );
 
